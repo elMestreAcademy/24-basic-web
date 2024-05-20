431e6b418fd5ff13fd3f90a2232e1e3ef1ce49f3
Carlos Mestre <carmesce@gmail.com>
2024-05-20 13:29:31 +0200

    initial commit

---
+++ patatascript.js
@@ -0,0 +1,14 @@
+document.getElementById("potatitulo").classList.add("titular");
+document.getElementById("potatexto").classList.add("titular");
+document.getElementById("ide").classList.add("rojo");
+
+/*var element = document.getElementById("potatitulo");
+element.classList.add("titular");
+*/
+
+
+
+
+
+
+
