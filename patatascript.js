3bfc590710526271ddebbf41ed4eb3d3ea11efe2
Carlos Mestre <carmesce@gmail.com>
2024-05-21 09:25:24 +0200

    Cambiar contenido de items con JS

--- patatascript.js
+++ patatascript.js
@@ -1,1 +1,4 @@
 document.getElementById("potatitulo").classList.add("titular");
+document.getElementById("flex-item-1").innerHTML = "Mariano";
+document.getElementById("flex-item-2").innerHTML = "Marta";
+document.getElementById("flex-item-3").innerHTML = "Marcos";
