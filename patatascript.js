3c630a23d41135f5ceffb2eb0d2975e55c2264c9
Carlos Mestre <carmesce@gmail.com>
2024-05-21 08:52:02 +0200

    Code Style

--- patatascript.js
+++ patatascript.js
@@ -1,14 +1,3 @@
 document.getElementById("potatitulo").classList.add("titular");
 document.getElementById("potatexto").classList.add("titular");
 document.getElementById("ide").classList.add("rojo");
-
-/*var element = document.getElementById("potatitulo");
-element.classList.add("titular");
-*/
-
-
-
-
-
-
-
