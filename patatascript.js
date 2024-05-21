f6c2c4f07ef015a4f4e3b4a05dfd607c044f8e20
Carlos Mestre <carmesce@gmail.com>
2024-05-21 09:14:45 +0200

    container with 3 items

--- patatascript.js
+++ patatascript.js
@@ -1,3 +1,1 @@
 document.getElementById("potatitulo").classList.add("titular");
-document.getElementById("potatexto").classList.add("titular");
-document.getElementById("ide").classList.add("rojo");
